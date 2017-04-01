8d4700bd2194408d93c5d4907fccfb63ec42035c
YAGolubev <yagolubev@inbox.com>
2017-04-02 00:30:39 +0400

    - delete article php

--- Article.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(empty($_GET['id'])){
-    header("Location: /");
-}
-
-require_once __DIR__ . '/autoload.php';
-$article = \App\Models\Article::findById($_GET['id']);
-include __DIR__ . '/templates/article.php';
